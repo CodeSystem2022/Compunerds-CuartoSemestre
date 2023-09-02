43020c94774661b11bf7eff551b7ba530721f74c
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-09-02 03:52:51 -0300

    Fix importaciones capa_dato_persona + renombre carpetas

--- Python/_continuacionSemestre03/capa_datos_persona/usuario_dao.py
+++ Python/_continuacionSemestre03/capa_datos_persona/usuario_dao.py
@@ -1,8 +1,13 @@
+from cursor_del_pool import CursorDelPool
+from logger_base import log
+from usuario import Usuario
+
+
 class UsuarioDAO:
-    ''' 
-    DAO → Data Acess Object para la tabla de usuario
+    """
+    DAO → Data Access Object para la tabla de usuario
     CRUD → Create - Read - Update - Delete
-    '''
+    """
 
     _SELECT = 'SELECT * FROM usuario ORDER BY id_usuario'
     _INSERTAR = 'INSERT INTO usuario(username, password) VALUE (%s, %s)'
@@ -10,7 +15,7 @@ class UsuarioDAO:
     _ELIMINAR = 'DELETE FROM usuario WHERE id_usuario=%s'
 
     @classmethod
-    def seleccionar (cls):
+    def seleccionar(cls):
         with CursorDelPool() as cursor:
             log.debug('Seleccionando usuarios')
             cursor.execute(cls._SELECT)
@@ -25,7 +30,7 @@ class UsuarioDAO:
     def insertar(cls, usuario):
         with CursorDelPool() as cursor:
             log.debug(f'Usuario a insertar: {usuario}')
-            valores = (usuarios.username, usuario.password)
+            valores = (usuario.username, usuario.password)
             cursor.execute(cls._INSERTAR, valores)
             return cursor.rowcount
 
@@ -33,7 +38,7 @@ class UsuarioDAO:
     def actualizar(cls, usuario):
         with CursorDelPool() as cursor:
             log.debug(f'Usuario a actualizar: {usuario}')
-            valores = (usuarios.username, usuario.password, usuario.id_usuario)
+            valores = (usuario.username, usuario.password, usuario.id_usuario)
             cursor.execute(cls._ACTUALIZAR, valores)
             return cursor.rowcount
 
@@ -45,23 +50,24 @@ class UsuarioDAO:
             cursor.execute(cls._ELIMINAR, valores)
             return cursor.rowcount
 
+
 if __name__ == '__main__':
-    #Eliminar usuario
+    # Eliminar usuario
     usuario = Usuario(id_usuario=3)
     usuario_eliminado = UsuarioDAO.eliminar(usuario)
     log.debug(f'Usuario eliminado: {usuario_eliminado}')
 
-    #Actualizar usuario
+    # Actualizar usuario
     usuario = Usuario(id_usuario=1, username='', password='369')
     usuario_modificado = UsuarioDAO.actualizar(usuario)
     log.debug(f'Usuario modificado: {usuario_modificado}')
 
-    #Insertar usuario
+    # Insertar usuario
     usuario = Usuario(username='kely', password='321')
     usuario_insertado = UsuarioDAO.insertar(usuario)
     log.debug(f'Usuario insertado: {usuario_insertado}')
 
-    #Listar o seleccionar
-    usarios = UsuarioDAO.seleccionar()
+    # Listar o seleccionar
+    usuarios = UsuarioDAO.seleccionar()
     for usuario in usuarios:
         log.debug(usuario)

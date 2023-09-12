5e111cbe057270e83acb7d259216ffaff14b5b91
carlafaes <carlafaes18@hotmail.com>
2023-09-12 12:28:14 -0300

    primeras modificaciones de metodos

--- Java/Sistema Estudiantes con Spring Boot/estudiantes/src/main/java/utn/servicio/EstudianteServicio.java
+++ Java/Sistema Estudiantes con Spring Boot/estudiantes/src/main/java/utn/servicio/EstudianteServicio.java
@@ -1,18 +1,25 @@
 package utn.servicio;
 
 import utn.estudiantes.modelo.Estudiante;
+import utn.repositorio.EstudianteRepositorio;
 
 import java.util.List;
 
+@Service 
 public class EstudianteServicio implements IEstudianteServicio {
+    @Autowired 
+    private EstudianteRepositorio estudianteRepositorio;
+
     @Override
     public List<Estudiante> listarEstudiantes() {
-        return null;
+        List<Estudiante> estudiantes= estudianteRepositorio.findAll();
+        return estudiantes;
     }
 
     @Override
     public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
-        return null;
+        Estudiante estudiante= estudianteRepositorio.findById(idEstudiante).orElse(null);
+        return estudiante;
     }
 
     @Override

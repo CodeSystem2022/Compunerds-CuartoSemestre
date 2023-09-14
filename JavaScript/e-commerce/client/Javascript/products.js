848e8a33f065a1dbf590d90e657e47fe218c401c
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-09-14 01:18:39 -0300

    add clase 3 js

--- JavaScript/e-commerce/client/Javascript/products.js
+++ JavaScript/e-commerce/client/Javascript/products.js
@@ -1,37 +1,37 @@
-const productos = [
-  {
-    id: 1,
-    productName: "Banana",
-    price: 480,
-    quanty: 1,
-    img: "/e-commerce/client/media/bananas.jpeg",
-  },
-  {
-    id: 2,
-    productName: "pollo",
-    price: 1480,
-    quanty: 1,
-    img: "/e-commerce/client/media/pollo.jpg",
-  },
-  {
-    id: 3,
-    productName: "leche",
-    price: 280,
-    quanty: 1,
-    img: "/e-commerce/client/media/leche.jpg",
-  },
-  {
-    id: 4,
-    productName: "Mayonesa",
-    price: 760,
-    quanty: 1,
-    img: "/e-commerce/client/media/mayonesa.jpg",
-  },
-  {
-    id: 5,
-    productName: "Enlatados",
-    price: 370,
-    quanty: 1,
-    img: "/e-commerce/client/media/enlatados.jpg",
-  },
+const productos =[
+    {
+        id:1,
+        productName: "Banana",
+        price : 480,
+        quanty: 1,
+        img: "/JavaScript/e-commerce/client/media/bananas.jpeg",
+    },
+    {
+        id:2,
+        productName: "Leche",
+        price : 1480,
+        quanty: 1,
+        img:"/JavaScript/e-commerce/client/media/leche.jpg",
+    },
+    {
+        id:3,
+        productName: "Pollo",
+        price : 280,
+        quanty: 1,
+        img:"/JavaScript/e-commerce/client/media/pollo.jpg",
+    },
+    {
+        id:4,
+        productName: "Mayonesa",
+        price : 760,
+        quanty: 1,
+        img:"/JavaScript/e-commerce/client/media/mayonesa.jpg",
+    },
+    // {
+    //     id: 5,
+    //     productName: "Enlatados",
+    //     price: 370,
+    //     quanty: 1,
+    //     img: "/client/media/Enlatados.jpg",
+    //   },
 ];

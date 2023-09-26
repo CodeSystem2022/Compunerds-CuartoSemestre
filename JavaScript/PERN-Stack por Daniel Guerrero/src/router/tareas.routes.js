577b2eed46d0e7971a1ee9625eb60bbb4f9c3dcb
jsegovia <jsegovia@boxcustodia.com>
2023-09-26 00:10:47 -0300

    Agrego clase05 de JS

---
+++ JavaScript/PERN-Stack por Daniel Guerrero/src/router/tareas.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { actualizarTarea, crearTarea, eliminarTarea, listarTareas } from "../controllers/tareas.controller.js";
+
+const router = Router();
+
+router.get("/tareas", listarTareas);
+
+router.get("/tareas/:id", listarTareas);
+
+router.post("/tareas", crearTarea);
+
+router.put("/tareas/:id", actualizarTarea);
+
+router.delete("/tareas/:id", eliminarTarea);
+
+export default router;

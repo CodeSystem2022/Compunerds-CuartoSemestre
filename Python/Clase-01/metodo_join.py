ecb23196f5b09b6b81d35594030ba508616ab955
carlafaes <carlafaes18@hotmail.com>
2023-10-05 00:56:42 -0300

    metodo join

---
+++ Python/Clase-01/metodo_join.py
@@ -0,0 +1,7 @@
+#help(str.join) ##    La cadena cuyo método se llama se inserta entre cada cadena dada.
+#El resultado se devuelve como una nueva cadena.
+#Ejemplo: '.'.join(['ab', 'pq', 'rs']) -> 'ab.pq.rs'
+
+tupla_str=('Hola','alumnos','Tecnicatura','Universitaria')
+mensaje=' '.join(tupla_str)
+print(mensaje)

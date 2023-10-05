4561ec29aa72b198f7959e4b35c869594991f6dc
carlafaes <carlafaes18@hotmail.com>
2023-10-05 00:18:43 -0300

    mi clase

---
+++ Python/Clase-01/mi_clase.py
@@ -0,0 +1,11 @@
+class MiCLase:
+    '''Este es un ejemplo de la documentacion de esta clase.
+    Es solo para saber que aqui esta MiClase'''
+    def __init__(self):
+        '''Aqui esta el metodo dunder init. Este metiodo es el de inicio para esta clase'''
+        
+    def mi_metodo(self,param1,param2):
+        '''
+        :param param1: este es el parametro 1
+        :param param2: este es el parametro 2
+        return: este es el valor de retorno'''

6a5e60917d32d789a384ea773b9cc08e4555cbd8
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-10-17 12:05:31 -0300

    modified: LibroForm.java

--- Java/Tienda Libros con SpringBoot y Swing/src/main/java/utn/tienda_libros/vista/LibroForm.java
+++ Java/Tienda Libros con SpringBoot y Swing/src/main/java/utn/tienda_libros/vista/LibroForm.java
@@ -1,14 +1,17 @@
 package utn.tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import java.awt.*;
 
+@Component
 public class LibroForm extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
-
+  
     @Autowired
     public LibroForm(LibroServicio libroServicio){
         this.libroServicio = libroServicio;
@@ -20,5 +23,11 @@ public class LibroForm extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900, 700);
+        // para obtener las dimenciones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getHeight()/2);
+        setLocation(x, y);        
     }
 }

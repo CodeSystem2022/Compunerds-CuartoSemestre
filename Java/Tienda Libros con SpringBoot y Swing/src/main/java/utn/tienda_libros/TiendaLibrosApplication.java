6f70aaf114aa8f14bf42f1f417c3e75321358622
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-10-17 12:21:51 -0300

    modified: TiendaLibrosApplication.java

--- Java/Tienda Libros con SpringBoot y Swing/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Tienda Libros con SpringBoot y Swing/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,14 +1,30 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroForm;
+
+
+import java.awt.EventQueue;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
+    public static void main(String[] args) {
+	
         ConfigurableApplicationContext contextoSpring = 
                 new SpringApplicationBuilder(TiendaLibrosApplication.class);
+                    .headless(false)
+                    .web(WebApplicationType.NONE)
+                    .run(args);
+        //Ejecutamos el código para cargar el formulario
+        EventQueue.invokeLater(() -> {
+            //Obtener el objeto from a traves del spring
+            LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+            libroForm.setVisible(true);
+        });
+    }
 }

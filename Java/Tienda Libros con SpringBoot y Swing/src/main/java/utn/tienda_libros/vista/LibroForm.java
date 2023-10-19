c592d596bae4ec143c1095db665118dbc1765cdb
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-10-19 14:22:39 -0300

    modified: LibroForm.form y LibroForm.java

--- Java/Tienda Libros con SpringBoot y Swing/src/main/java/utn/tienda_libros/vista/LibroForm.java
+++ Java/Tienda Libros con SpringBoot y Swing/src/main/java/utn/tienda_libros/vista/LibroForm.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 @Component
 public class LibroForm extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
+    private JTable tablaLibros;
+    private DefaultTableModel tablaModeloLibros;
   
     @Autowired
     public LibroForm(LibroServicio libroServicio){
@@ -29,5 +32,33 @@ public class LibroForm extends JFrame {
         int x = (tamanioPantalla.width - getWidth()/2);
         int y = (tamanioPantalla.height - getHeight()/2);
         setLocation(x, y);        
+    }
+
+    private void createUIComponents(){
+        this.tablaModeloLibros = new DefaultTableModel(o, 5);
+        String[] cabecera = {"Id", "Libro", "Autor", "Precio", "Existencias"};
+        this.tablaModeloLibros.setColumnIdentifiers(cabecera);
+        // Instanciar el objeto de JTable
+        this.tablaLibros = JTable(tablaModeloLibros);
+        listarLibros();
+    }
+
+    private void listarLibros(){
+        //Limpiar la tabla
+        tablaModeloLibros.setRowCount(0);
+        //Obtener los libros de la BD
+        var libros = libroServicio.listarLibros();
+        //Iteramos cada libro
+        libros.forEach((libro) -> { //función lambda
+            // creamos cada registro para agregarlos a la tabla
+            Object [] renglonLibro = {
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias()
+            };
+            this.tablaModeloLibros.addRow(renglonLibro);
+        });
     }
 }
